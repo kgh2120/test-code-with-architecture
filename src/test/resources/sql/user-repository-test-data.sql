1553ba72263dafab4d7e4c82b9d86ac612965bab
kyoohyun <kgh2120@naver.com>
2023-10-23 00:18:57 +0900

    [test] add UserRepositoryTest

---
+++ src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values ('1', 'kgh2120@naver.com', 'kgh2120', 'Seoul', 'aaaa', 'ACTIVE', 0);
