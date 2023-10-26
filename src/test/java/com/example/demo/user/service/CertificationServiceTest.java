a0940f4406eb3220ea41a33ccf39b393e090a91b
kyoohyun <kgh2120@gmail.com>
2023-10-27 08:13:12 +0900

    외부연동 파트 完

---
+++ src/test/java/com/example/demo/user/service/CertificationServiceTest.java
@@ -0,0 +1,30 @@
+package com.example.demo.user.service;
+
+import com.example.demo.mock.FakeMailSender;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CertificationServiceTest {
+
+    @Test
+    void 이메일과_컨텐츠가_제대로_만들어져서_보내지는지_테스트한다(){
+        //given
+        FakeMailSender mailSender = new FakeMailSender();
+        CertificationService certificationService = new CertificationService(mailSender);
+
+
+        //when
+        long userId = 1L;
+        String certificationUrl = "aaaa-aaaa-aaaa-aaa";
+        String email = "foo@bar.com";
+        certificationService.send(email, userId, certificationUrl);
+
+        //then
+        assertThat(mailSender.email).isEqualTo("foo@bar.com");
+        assertThat(mailSender.title).isEqualTo("Please certify your email address");
+        assertThat(mailSender.content).isEqualTo("Please click the following link to certify your email address: " + "http://localhost:8080/api/users/" + userId + "/verify?certificationCode=" + certificationUrl);
+    }
+
+}
